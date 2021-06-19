377cbd85c26d77d9049e64100bb65e2bbd8fc38c
datalessuk <dataless@gmail.com>
2021-06-19 16:55:35 +0100

    styled comp

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,43 @@
-import '../styles/search.scss'
+//import '../styles/search.scss'
+import styled from "styled-components";
+import seatchIcon from '../img/icons/search-solid.svg'
 
 function Search(){
+
+    const SearchDiv = styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 50px;
+    `;
+    const SearchInput = styled.input`
+        border-radius: 20px;
+        border: none;
+        padding: 20px 15px 15px 20px;
+        height: 25px;
+        width: auto;
+        outline: none;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        width: 450px;
+        font-family: 'Montserrat', sans-serif;
+        text-align: center;
+
+    /*Seach Icon on left side */
+        background-image: url(${seatchIcon});
+        background-repeat: no-repeat;
+        background-position: 4px 10px;
+        background-size: 15px 15px;
+    `;
+
+    
+    //<input type="text" className="search-bar" placeholder="Enter location"></input>
     
 
     return(
-        <div className="search-container">
-            <input type="text" className="search-bar" placeholder="Enter location"></input>
+        <div>
+            <SearchDiv>
+            <SearchInput placeholder="Enter location" type="text"></SearchInput>
+            </SearchDiv>
         </div>
     )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,31 @@
-import '../styles/welcome.css'
-
+//import '../styles/welcome.css'
+import styled from "styled-components";
 function Welcome(){
+
+    const Title = styled.h1`
+    font-family: 'Montserrat', sans-serif;
+    color:white;
+    letter-spacing:3px;
+    margin-top:30px;
+    `;
+
+    const TitleDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    `;
+
+   
+
     return(
-        <div className="heading-container">
-            
-            <h1>Weather App</h1>
-        </div>
+        <div>
+        <TitleDiv>
+            <Title>
+                Weather App
+            </Title>
+            </TitleDiv>
+            </div>
     )
 }
 export default Welcome;
